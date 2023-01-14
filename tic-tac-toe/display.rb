dd02f6f3caa1e84fbbdd85acec7751ff12312905
redockirtap <redockirtap@gmail.com>
2023-01-14 14:32:18 +0200

    Work on console game tic-tac-toe.

---
+++ tic-tac-toe/display.rb
@@ -0,0 +1,11 @@
+
+
+puts "   1 | 2 | 3 " 
+puts "A ___|___|___" 
+puts "B ___|___|___"
+puts "C    |   |   "
+
+puts "   1   2   3 " 
+puts "A _X_|_O_|_O_" 
+puts "B ___|_X_|_O_"
+puts "C    |   | X "
---
+++ tic-tac-toe/game.rb
@@ -0,0 +1,16 @@
+
+class ChessBoard
+	def initialize
+		@
+	end
+end
+
+class Player
+	def initialize(name)
+		@name = name
+	end
+end
+
+first_player = Player.new("Player 1")
+second_player = Player.new("Player 2")
+
