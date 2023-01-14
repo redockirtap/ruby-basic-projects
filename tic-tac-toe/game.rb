8894e30070f5dbda6b94592158cec5232d65c4f1
redockirtap <redockirtap@gmail.com>
2023-01-14 16:08:02 +0200

    Add ChessBoard and Player classes. Sketching the logic.

--- tic-tac-toe/game.rb
+++ tic-tac-toe/game.rb
@@ -1,16 +1,32 @@
 
 class ChessBoard
+
+	@@board = ["__", "__", "__",
+	"__", "__", "__",
+	"__", "__", "__"]
+
 	def initialize
-		@
+		#
+	end
+
+	# def self.board
+	# 	@@board
+	# end
+
+	def self.add_marker
+		@@board[0] = "A"
 	end
 end
 
 class Player
-	def initialize(name)
+	def initialize(name, marker)
 		@name = name
+		@marker = marker
 	end
 end
 
-first_player = Player.new("Player 1")
-second_player = Player.new("Player 2")
+first_player = Player.new("Player 1", "X")
+second_player = Player.new("Player 2", "O")
 
+p ChessBoard.add_marker
+p ChessBoard.board
