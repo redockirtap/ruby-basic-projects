9bd5646f139c1bda2dd61106b58ce12546ce136d
redockirtap <redockirtap@gmail.com>
2023-01-15 19:11:10 +0200

    Add win check conditions to #win? method.

---
+++ tic-tac-toe/controller.rb
@@ -0,0 +1,51 @@
+class Controller
+	@@first_player = nil
+	@@second_player = nil
+	@@current_marker = nil
+
+	def self.start_game
+		puts "Choose a marker for the first Player."
+		@@first_player = Player.new("Player 1", gets.chomp)
+
+		puts "Choose a marker for the second Player."
+		@@second_player = Player.new("Player 2", gets.chomp)
+	end
+
+	def self.change_turn 
+		if @@current_marker == @@first_player.marker
+			@@current_marker = @@second_player.marker
+		else
+			@@current_marker = @@first_player.marker
+		end
+	end
+
+	def self.win?
+		p GameBoard.board[0..2].all?(@@current_marker) # top row
+		p GameBoard.board[3..5].all?(@@current_marker) # middle row
+		p GameBoard.board[6..8].all?(@@current_marker) # bottom row
+
+		p GameBoard.board[(0..).step(3)].all?(@@current_marker) # first column
+		p GameBoard.board[(1..).step(3)].all?(@@current_marker) # second column
+		p GameBoard.board[(2..).step(3)].all?(@@current_marker) # third column
+
+		p GameBoard.board[(0..).step(4)].all?(@@current_marker) # first diagonal
+		p GameBoard.board[(2..).step(2)].all?(@@current_marker) # second diagonal
+
+		p @@current_marker
+	end
+
+	def self.draw?
+		GameBoard.board.none?(nil)
+	end
+
+	def self.freecell?(cell)
+		GameBoard.board[cell] == nil
+	end
+
+	def self.add_marker(marker)
+		puts "Choose a cell to place #{marker} marker."
+		cell = gets.chomp.to_i
+		GameBoard.add_marker(marker, cell)
+	end
+
+end
--- tic-tac-toe/game.rb
+++ tic-tac-toe/game.rb
@@ -21,7 +21,7 @@ class GameBoard
 end
 
 class Player
-	attr_reader :marker
+	attr_reader :name, :marker
 
 	def initialize(name, marker)
 		@name = name
@@ -30,8 +30,9 @@ class Player
 end
 
 class Controller
-	@@first_player = "X"
-	@@second_player = "Y"
+	@@first_player = nil
+	@@second_player = nil
+	@@current_marker = 'X'
 
 	def self.start_game
 		puts "Choose a marker for the first Player."
@@ -41,24 +42,41 @@ class Controller
 		@@second_player = Player.new("Player 2", gets.chomp)
 	end
 
-	def self.win?
-		p GameBoard.board[0..2].all?(marker) 
+	def self.change_turn 
+		if @@current_marker == @@first_player.marker
+			@@current_marker = @@second_player.marker
+		else
+			@@current_marker = @@first_player.marker
+		end
+	end
 
+	def self.win?
+		p GameBoard.board[0..2].all?(@@current_marker) # top row
+		p GameBoard.board[3..5].all?(@@current_marker) # middle row
+		p GameBoard.board[6..8].all?(@@current_marker) # bottom row
+
+		p GameBoard.board[(0..).step(3)].all?(@@current_marker) # first column
+		p GameBoard.board[(1..).step(3)].all?(@@current_marker) # second column
+		p GameBoard.board[(2..).step(3)].all?(@@current_marker) # third column
+
+		p GameBoard.board[(0..).step(4)].all?(@@current_marker) # first diagonal
+		p GameBoard.board[(2..).step(2)].all?(@@current_marker) # second diagonal
+
+		p @@current_marker
 	end
 
 	def self.draw?
-
+		GameBoard.board.none?(nil)
 	end
 
-	def self.freecell?
-
+	def self.freecell?(cell)
+		GameBoard.board[cell] == nil
 	end
 
 	def self.add_marker(marker)
-		puts "choose a cell to place your marker."
+		puts "Choose a cell to place #{marker} marker."
 		cell = gets.chomp.to_i
 		GameBoard.add_marker(marker, cell)
-		GameBoard.board.map.with_index { |marker, cell| p marker, cell+1 }
 	end
 
 end
@@ -69,3 +87,4 @@ Controller.add_marker("X")
 Controller.add_marker("X")
 
 p Controller.win?
+p GameBoard.board
