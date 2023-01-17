de1f0218a7119d42377cdc2442b54a85cc6e3ade
redockirtap <redockirtap@gmail.com>
2023-01-17 18:32:41 +0200

    Refactoring code.

--- tic-tac-toe/controller.rb
+++ tic-tac-toe/controller.rb
@@ -1,22 +1,19 @@
 # frozen_string_literal: true
 
-# Game logic module with methods to check win or draw, to add marker
+# Game logic module with methods to start and restart the game,
+# to check win or draw, to add markers to the board
 class Controller
-  def self.run_game
-    @@first_player = 'X'
-    @@second_player = 'O'
-    @@current_marker = nil
-    @@cell = nil
+  @@first_player = 'X'
+  @@second_player = 'O'
+  @@current_marker = nil
+  @@cell = nil
 
+  def self.run_game
     until Controller.draw? || Controller.win?
-      p Controller.draw?, Controller.win?
       Controller.change_turn
       Controller.choose_cell
-      until Controller.freecell?(@@cell)
-        Controller.choose_cell
-      end
+      Controller.choose_cell until Controller.freecell?(@@cell)
       Controller.add_marker(@@current_marker)
-      # p GameBoard.board, Controller.win?, @@current_marker
       puts Display.display
     end
 
@@ -24,28 +21,32 @@ class Controller
   end
 
   def self.change_turn
-    if @@current_marker == @@first_player
-      @@current_marker = @@second_player
+    @@current_marker = if @@current_marker == @@first_player
+                         @@second_player
     else
-      @@current_marker = @@first_player
+      @@first_player
     end
   end
 
   def self.win?
-    # p GameBoard.board
-    case 
-    when GameBoard.board[0..2].all?(@@current_marker), # top row
-      GameBoard.board[3..5].all?(@@current_marker), # middle row
-      GameBoard.board[6..8].all?(@@current_marker), # bottom row
-      GameBoard.board[(0..).step(3)].all?(@@current_marker), # first column
-      GameBoard.board[(1..).step(3)].all?(@@current_marker), # second column
-      GameBoard.board[(2..).step(3)].all?(@@current_marker), # third column
-      GameBoard.board.values_at(0, 4, 8).all?(@@current_marker), # first diagonal
+    Controller.row_win? || Controller.col_win? || Controller.dia_win?
+  end
+
+  def self.row_win?
+    GameBoard.board[0..2].all?(@@current_marker) || # top row
+      GameBoard.board[3..5].all?(@@current_marker) || # middle row
+      GameBoard.board[6..8].all?(@@current_marker) # bottom row
+  end
+
+  def self.col_win?
+    GameBoard.board[(0..).step(3)].all?(@@current_marker) || # first column
+      GameBoard.board[(1..).step(3)].all?(@@current_marker) || # second column
+      GameBoard.board[(2..).step(3)].all?(@@current_marker) # third column
+  end
+
+  def self.dia_win?
+    GameBoard.board.values_at(0, 4, 8).all?(@@current_marker) || # first diagonal
       GameBoard.board.values_at(2, 4, 6).all?(@@current_marker) # second diagonal
-      true
-    else
-      false
-    end
   end
 
   def self.draw?
@@ -68,18 +69,9 @@ class Controller
 
   def self.end_game
     p Controller.draw?
-    # puts "#{@@current_marker} won! One more? [y/n]: "
     puts Controller.draw? ? "It's a draw. Another one? [y/n]: " : "#{@@current_marker} won! One more? [y/n]: "
     GameBoard.reset
     answer = gets.chomp.downcase
     answer == 'y' ? Controller.run_game : 'See you.'
   end
 end
-
-# class Display
-#   def self.display
-#     puts "_#{GameBoard.board[0]}_|_#{GameBoard.board[1]}_|_#{GameBoard.board[2]}_" 
-#     puts "_#{GameBoard.board[3]}_|_#{GameBoard.board[4]}_|_#{GameBoard.board[5]}_"
-#     puts " #{GameBoard.board[6]} | #{GameBoard.board[7]} | #{GameBoard.board[8]} "
-#   end
-# end
