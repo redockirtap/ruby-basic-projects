be36f1edcd758daf37d06a77e2e2c16de7d84c60
redockirtap <redockirtap@gmail.com>
2023-01-22 21:02:18 +0200

    Add modules for player and computer game logic.

--- mastermind/game.rb
+++ mastermind/game.rb
@@ -11,8 +11,9 @@ class ColorBoard
   def initialize
     @default_colors = %w[red green yellow pink blue gray]
     @hints = %w[close exact wrong]
-    @secret = []
-    @guesses = []
+    @secret = 1234
+    @guess = 4321 # optional
+    @guesses = [] # optional
   end
 
   def hints
@@ -30,6 +31,11 @@ class ColorBoard
   def add_secret(secret)
     @secret.push(secret)
   end
+
+  def check_guess(guess)
+    @guesses.push(guess) # optional
+    @guess
+  end
 end
 
 # Breaker of the code
@@ -41,15 +47,46 @@ end
 
 # Displaying the game in the console
 class Display
-  def initialize
-    @display
+  # rubocop: disable Metrics/ParameterLists, Naming/MethodParameterName
+  def initialize(c1, c2, c3, c4, h1, h2, h3, h4)
+    # c stands for color, h - for hint
+    @c1 = c1
+    @c2 = c2
+    @c3 = c3
+    @c4 = c4
+    @h1 = h1
+    @h2 = h2
+    @h3 = h3
+    @h4 = h4
+  end
+  # rubocop: enable Metrics/ParameterLists, Naming/MethodParameterName
+
+  def colorize
+    # method to colorize the game
+  end
+
+  def preview
+    puts %(
+      ╔═══╤═══╤═══╤═══╤═══╤═══╗  ╔═══╤═══╤═══╤═══╗
+      ║ 1 │ 2 │ 3 │ 4 │ 5 │ 6 ║  ║ 1 │ 2 │ 3 │ 4 ║
+      ╚═══╧═══╧═══╧═══╧═══╧═══╝  ╚═══╧═══╧═══╧═══╝
+    )
+  end
+
+  def display
+    puts %(
+      | Guess #1:        | Hint #1:
+      ╔═══╤═══╤═══╤═══╗  ╔═══╤═══╤═══╤═══╗
+      ║ 1 │ 2 │ 3 │ 4 ║  ║ 1 │ 2 │ 3 │ 4 ║
+      ╚═══╧═══╧═══╧═══╝  ╚═══╧═══╧═══╧═══╝
+    )
   end
 end
 
 # Game logic, presentaion control
 class GameController
   def initialize(codebreaker)
-    @turns = -1
+    @turns = 0
     @codebreaker = codebreaker
   end
 
@@ -71,6 +108,10 @@ end
 module PlayerBreaker
   def make_guess
     # make a guess
+    puts 'make a guess'
+    guess = gets.chomp
+    make_guess unless guess.match(/^[1-6]{4}$/)
+    guess
   end
 end
 
@@ -80,3 +121,7 @@ module ComputerBreaker
     # make a guess
   end
 end
+
+# rubocop: disable all
+
+
